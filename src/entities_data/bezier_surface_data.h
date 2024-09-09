7b83cdfc7f052dc1de448a42082fd176617d8a80
Manomenu <88382777+Manomenu@users.noreply.github.com>
2024-09-09 22:50:10 +0200

    ok version

---
+++ src/entities_data/bezier_surface_data.h
@@ -0,0 +1,13 @@
+#pragma once
+#include <config.h>
+
+struct BezierSurfaceData
+{
+    glm::vec3 position = glm::vec3(0);
+    glm::vec3 eulers = glm::vec3(0);
+    glm::vec3 scale = glm::vec3(1);
+
+    glm::vec3 color;
+    std::vector<std::vector<float>> control_points;
+    int density = 5;
+};
